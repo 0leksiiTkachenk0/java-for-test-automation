6febdf8a493f9057fba7982fe67032d3873dddac
0leksiiTkachenk0 <165346080+0leksiiTkachenk0@users.noreply.github.com>
2024-05-24 02:00:03 +0200

    Nested conditionals

--- src/test/java/com/serenitydojo/model/Feeder.java
+++ src/test/java/com/serenitydojo/model/Feeder.java
@@ -2,17 +2,37 @@ package com.serenitydojo.model;
 
 public class Feeder {
     public String feeds(String animal, boolean isPremium) {
-        if (animal.equals("Cat")) {
-//            if (isPremium) {
-//                return "Salmon";
-//            } else {
-//                return "Tuna";
-//            }
-            return (isPremium) ? "Salmon" : "Tuna";
-        } else if (animal.equals("Dog")) {
-            return "Dog Food";
-        } else {
-            return "Cabbage";
+
+        String food;
+
+        switch (animal) {
+            case "Cat":
+                food = (isPremium) ? "Salmon" : "Tuna";
+                break;
+            case "Dog":
+                food = (isPremium) ? "Deluxe Dog Food" : "Dog Food";
+                break;
+            case "Hamster":
+                food = (isPremium) ? "Lettuce" : "Cabbage";
+                break;
+            default:
+                food = "Don't know this animal - don't know the food";
         }
+
+        return food;
     }
 }
+
+        //        if (animal.equals("Cat")) {
+////            if (isPremium) {
+////                return "Salmon";
+////            } else {
+////                return "Tuna";
+////            }
+//            return (isPremium) ? "Salmon" : "Tuna";
+//        } else if (animal.equals("Dog")) {
+//            return "Dog Food";
+//        } else {
+//            return "Cabbage";
+//        }
+
